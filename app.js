36f79cd5a5e02f6def0856e5fda5b3973b21074e
francistobi <frobigwe305@gmail.com>
2023-09-06 07:25:05 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello World");
+    res.end();
+  }
+  if (req.url === "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3, 4]));
+    res.end();
+  }
+});
+
+server.listen(3000);
+
+console.log("listening on port 3000...");
